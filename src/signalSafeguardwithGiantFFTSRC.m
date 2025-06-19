2ae48b380a0619d5b9844f2c240a4600b29f0639
Hideki Kawahara <kawahara@kcn.ne.jp>
2025-06-20 05:14:07 +0900

    Update signalSafeguardwithGiantFFTSRC.m
    Made white noise function an internal function.

--- src/signalSafeguardwithGiantFFTSRC.m
+++ src/signalSafeguardwithGiantFFTSRC.m
@@ -200,9 +200,24 @@ if nargout == 2
     output.avPw = avPw;
     output.fc = fc;
     output.avPwi = 10.^(avPwi/10);
-    output.whiteDetNoise = whiteDetNoise(nData,fs);
+    output.whiteDetNoise = whiteDetNoiseInt(nData,fs);
     output.elapsedTime = toc(startTic);
 else
     y = xSg;
 end
+end
+
+function y = whiteDetNoiseInt(n,fs) % This function is for debug.
+%%
+yF = zeros(n,1);
+fx = (0:n-1)'/n*fs;
+tmp = fx(fx<=fs/2)*0+1;
+tmp = tmp.*exp(1i*2*pi*rand(length(tmp),1));
+tmp(1) = 1;
+yF(1:length(tmp)) = tmp;
+yF(end:-1:end-length(tmp)+2) = conj(tmp(2:end));
+if rem(n,2) == 0
+yF(n/2+1) = 1;
+end
+y = real(ifft(yF));
 end
